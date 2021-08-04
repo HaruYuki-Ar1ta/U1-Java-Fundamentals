dd7c8b8eedf6cc7c092362462f34d63c9df49897
HaruYuki-Ar1ta <Campbcha000@warren.k12.in.us>
2021-08-04 12:52:24 -0400

    Competed L1 and L2 Notes

--- src/L1.java
+++ src/L1.java
@@ -1,8 +1,9 @@
 public class L1 {
 
-    //You will notice that the class header is already define.
-
-    //add the main method header below.
-
-
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.print("Hello");
+        System.out.print("Worlds");
+        System.out.println("CS is awesome");
+    }
 }
--- src/L2.java
+++ src/L2.java
@@ -1,4 +1,22 @@
 public class L2 {
 
     //add the main method header
+    public static void main(String[] args){
+        double num1 = 3.4;
+        boolean num2 = true;
+        double num3 = 4.5;
+
+        String firstName = "George";
+        int age = 16;
+        System.out.println(firstName);
+        System.out.println(age);
+
+        String owner = "Blake";
+        int routingNumber = 123456789;
+        double balance = 0.99;
+        System.out.println(owner);
+        System.out.println(routingNumber);
+        System.out.println(balance);
+    }
+
 }
