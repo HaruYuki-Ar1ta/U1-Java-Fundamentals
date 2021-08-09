fd33b6e7db38c39adc59d3aa50a869191fe09182
HaruYuki-Ar1ta <Campbcha000@warren.k12.in.us>
2021-08-09 13:03:27 -0400

    Lessons 3,4, and 5

--- src/L4.java
+++ src/L4.java
@@ -1,2 +1,27 @@
+
 public class L4 {
+
+    public static void main(String[] args) {
+
+        int value = 5 ;
+
+        //Declare a variable stating the data type and identifier
+
+        double total;
+        String message = "Hi";
+        final double GRAVITY = -9.8;
+
+        //Assign a value
+
+        total = 6;
+
+        System.out.println(message + ", the value is "+ total);
+        //Reuse do not need to declare
+
+        total = 10;
+        message = "Bye";
+        //GRAVITY = -10; --Tiss is bad
+
+        System.out.println(message + ", the value is "+ total);
+    }
 }
