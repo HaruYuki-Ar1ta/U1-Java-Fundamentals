c126ca52e126f3b7f001f8ddbe4fbfc46aadf9f8
HaruYuki-Ar1ta <Campbcha000@warren.k12.in.us>
2021-08-10 13:17:37 -0400

    Averages Completed

--- Finding Averages/src/Averages.java
+++ Finding Averages/src/Averages.java
@@ -5,5 +5,23 @@ public class Averages {
     Example: 12, 13, 14, 15 -> 13.5
      */
 
+    public static void main(String[] args) {
+        int testScore1 = 129384;
+        int testScore2 = 13;
+        int testScore3 = 14234956;
+        int testScore4 = 15;
+
+        int sumScore = testScore1 + testScore2 + testScore3 + testScore4;
+        double average =  sumScore / 4.0;
+
+        System.out.println("The average is " + average);
+
+
+
+
+
+
+    }
+
 
 }
