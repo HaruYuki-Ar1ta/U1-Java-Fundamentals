ca0eb8ee657400c6cfe056a1708289f0e27a58a5
HaruYuki-Ar1ta <Campbcha000@warren.k12.in.us>
2021-08-26 13:15:14 -0400

    Lessons 8 and 9 Completed

--- src/L8.java
+++ src/L8.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 public class L8 {
 
     public static void main(String[] args) {
+
+        //Create a Scanner object to read input
+        Scanner read = new Scanner(System.in);
+        /*
         String name;
+
         int hours;
         double payRate;
         double grossPay;
 
-        //Create a Scanner object to read input
-        Scanner read = new Scanner(System.in);
 
         //Get the user's name
         System.out.println("What is your name?");
@@ -28,5 +31,37 @@ public class L8 {
 
         System.out.println("Hello " + name);
         System.out.println("Your gross pay is $" + grossPay);
+        */
+
+        //Bugaboo #1 - Hanging Carriage
+       /* int age;
+        String name;
+
+        System.out.println("How old are you?");
+        age = read.nextInt();
+        read.nextLine(); //Eats up the carriage return
+
+        System.out.println("What is your name?");
+        name = read.nextLine();
+
+        System.out.println("What is your last name?");
+        String lastName = read.nextLine();
+
+        System.out.println("Hello, " + name + lastName +" . You are " + age + " years old.");
+    */
+    // Bugaboo 2 - Tokens
+    /*    String firstName;
+        String lastName;
+
+        System.out.println("enter your full name: (all on one line, press Enter when done))");
+        firstName = read.next();
+        lastName = read.nextLine();
+
+        System.out.println("Hello, " + firstName + " " + lastName);
+
+     */
+
+
     }
+
 }
